be0ee36100410f50902223afcfac5b8551ded793
Elena <inculet98@mail.ru>
2023-10-10 18:32:21 +0300

    adaugare modificari

---
+++ index.js
@@ -0,0 +1,102 @@
+// const arr = [1, 2, 3, 4, 5]
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i)
+//   i++
+// }
+
+
+// const arr = [1, 2, 3, 4, 5]
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i)
+// }
+
+
+// const arr = [1, 2, 3, 4, 5]
+// let i = 0;
+// do {
+//   console.log(i++)
+// } while (i <= 10)
+
+
+// const arr = new Array(1, 2, 3, 4, 5)
+// console.log(arr)
+
+
+// const arr = [1, 2, 3, 4, 5]
+// console.log(arr.map((item) => {
+//   return {
+//     item
+//   }
+// }))
+
+
+// const arr = [1, 2, 3, 4, 5]
+// const arr1 = arr.filter((item, index) => {
+//   return index > 3
+// })
+// console.log('arr', arr)
+// console.log('arr1', arr1)
+
+
+// const arr = ['a', 'b', 'c', 'd', 'e']
+// const idx = arr.indexOf('c')
+// console.log('arr', arr)
+// console.log('arr1', idx)
+
+
+// const arr = ['a', 'b', 'c', 'd', 'e']
+// const idx = arr.findIndex((item) => {
+//   return item > 'c'
+// })
+// console.log('arr', arr)
+// console.log('arr1', idx)
+
+
+// const arr = [1, 2, 3, 4, 5]
+// const every = arr.every((item) => {
+//   return item >= 10
+// })
+// console.log('arr', arr)
+// console.log('arr1', every)
+
+
+// const arr = [3, 10, 18, 20, 30]
+// const some = arr.some((item) => {
+//   return item >= 18
+// })
+
+// console.log('arr', arr)
+// console.log('arr1', some)
+
+
+// const arr = [[18, 21], 30, [43, [[55]]]]
+
+// console.log('arr', arr)
+// console.log('flat', arr.flat(3))
+
+
+// const fruits = ['strawberry', 'orange', 'apple']
+// fruits.forEach((element) => console.log(element));
+
+
+// const arr = [15, 18, 20, 100, 150]
+// const found = arr.find((element) => element > 10);
+// console.log(found)
+
+
+// const arr = ['a', 'b', 'c', 'd']
+// const arr1 = ['e', 'f', 'g', 'h']
+// const arr2 = arr.concat(arr1);
+// console.log(arr2)
+
+
+// const fruits = ['apple', 'orange', 'strawberry']
+// const vegetables = ['tomato', 'potatoes', 'carrots']
+// const produce = [...fruits, ...vegetables]
+// console.log(produce)
+
+
+// const fruits = ['apple', 'orange', 'strawberry']
+
+// console.log(fruits.includes('apple'));
